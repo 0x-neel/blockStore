3568ec9d1dec0d9def31489c7892226a0208e982
0x-neel <121809791+0x-neel@users.noreply.github.com>
2023-01-02 20:52:32 +0530

    Update VideoCard.tsx

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -91,7 +91,7 @@ export default function VideoCard(props: TestimonialCardProps) {
               };
               //Load above config in a window
               window.location.replace(
-                "https://app.unlock-protocol.com/checkout?paywallConfig=%7B%22locks%22%3A%7B%220xc37ffe60f6c3830ed0e92939d41ad1ecf8fd46d9%22%3A%7B%22network%22%3A5%2C%22skipRecipient%22%3Atrue%7D%7D%2C%22pessimistic%22%3Atrue%2C%22skipRecipient%22%3Atrue%7D"
+                "https://app.unlock-protocol.com/checkout?paywallConfig=%7B%22locks%22%3A%7B%220xc9c5ad79e13f0fd5a11001f84c4271feef92e806%22%3A%7B%22network%22%3A5%7D%7D%2C%22pessimistic%22%3Atrue%2C%22skipRecipient%22%3Atrue%2C%22redirectUri%22%3A%22https%3A%2F%2Fblock-store-zeta.vercel.app%2Fplay%22%2C%22persistentCheckout%22%3Afalse%2C%22referrer%22%3A%22%22%2C%22messageToSign%22%3A%22%22%2C%22hideSoldOut%22%3Afalse%7D"
               );
             }}
           >
